738f4d01aebaf7c45fafb9cb22e425b48dc93610
BleckSet <108376728+BleckSet@users.noreply.github.com>
2023-10-21 11:33:25 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -72,3 +72,25 @@ $(".burger").click(function() {
   $(".burger").toggleClass("active");
   $("html").toggleClass("hold");
 });
+
+
+ gsap.registerPlugin(ScrollTrigger);
+  
+  const fadeElements = document.querySelectorAll('.appearance');
+  
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+        observer.unobserve(entry.target);
+      } else {
+        entry.target.classList.remove('visible');
+      }
+    });
+  }, {
+    threshold: 0.3 // Порог пересечения 0.5 означает, что элемент считается видимым, если видно 50% его области
+  });
+  
+  fadeElements.forEach((fadeElement) => {
+    observer.observe(fadeElement);
+  });
